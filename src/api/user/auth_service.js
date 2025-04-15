16d402c3786f2cf43e9d54f81359d5439317da6c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-15 12:43:21 -0300

    feat: add errors for auth service

--- src/api/user/auth_service.js
+++ src/api/user/auth_service.js
@@ -5,4 +5,10 @@ const User = require('./user')
 const env = require('../../.env')
 
 const emailRegex = /\S+@\S+\.\S+/
-const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/const
+
+sendErrorsFromDB = (res, dbErrors) => {
+    const errors = []
+    _.forIn(dbErrors.errors, error => errors.push(error.message))
+    return res.status(400).json({ errors })
+}
